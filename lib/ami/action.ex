8fae9a2186d60b5091ba8d045fcc69677e5c3335
Stas Kobzar <stas.kobzar@clearlyip.com>
2021-12-23 20:18:23 -0500

    AMI action

---
+++ lib/ami/action.ex
@@ -0,0 +1,75 @@
+defmodule AMI.Action do
+  def new(action) when is_bitstring(action) do
+    new(action, [])
+  end
+
+  def new(action, list)
+      when is_bitstring(action) and is_list(list) do
+    action = String.trim(action)
+
+    cond do
+      String.length(action) == 0 ->
+        {:invalid}
+
+      true ->
+        map = %{"Action" => [action]}
+        create(map, list)
+    end
+  end
+
+  def new(_action, _list) do
+    {:invalid}
+  end
+
+  defp create(%{} = map, [h | tail]) do
+    {k, v} = h
+
+    case add_field(map, k, v) do
+      {:ok, map} -> create(map, tail)
+      _ -> {:invalid}
+    end
+  end
+
+  defp create(%{} = map, []) do
+    if Map.has_key?(map, "ActionID") do
+      {:ok, map}
+    else
+      add_field(map, "ActionID", action_id())
+    end
+  end
+
+  def add_field(_map, "", _value) do
+    {:invalid}
+  end
+
+  def add_field(%{} = map, name, value)
+      when is_bitstring(name) and is_bitstring(value) do
+    map =
+      case Map.fetch(map, name) do
+        {:ok, val} -> Map.put(map, name, [value | val])
+        _ -> Map.put(map, name, [value])
+      end
+
+    {:ok, map}
+  end
+
+  def to_string(%{} = map) do
+    map
+    |> Enum.map(fn {k, v} ->
+      Enum.map(v, fn h -> ~s(#{k}: #{h}) end)
+      |> Enum.reverse()
+    end)
+    |> List.flatten()
+    |> List.foldr("\r\n", fn h, acc -> ~s(#{h}\r\n) <> acc end)
+  end
+
+  defp action_id() do
+    base = Enum.to_list(?a..?z) ++ Enum.to_list(?0..?9)
+
+    List.foldr(
+      Enum.to_list(0..24),
+      "",
+      fn _, acc -> acc <> <<Enum.random(base)>> end
+    )
+  end
+end
---
+++ test/ami/action_test.exs
@@ -0,0 +1,90 @@
+defmodule AMI.ActionTest do
+  use ExUnit.Case, async: true
+
+  test "#add_field" do
+    map = %{}
+    assert {:ok, map} = AMI.Action.add_field(map, "ActionID", "FOO-0feed123")
+    assert {:ok, map} = AMI.Action.add_field(map, "Variable", "Bar")
+    assert {:ok, map} = AMI.Action.add_field(map, "Value", "1")
+
+    %{"ActionID" => [val]} = map
+    assert val == "FOO-0feed123"
+    %{"Variable" => [val]} = map
+    assert val == "Bar"
+    %{"Value" => [val]} = map
+    assert val == "1"
+
+    assert {:ok, map} = AMI.Action.add_field(map, "Value", "2")
+    assert {:ok, map} = AMI.Action.add_field(map, "Value", "3")
+    %{"Value" => val} = map
+    assert val == ["3", "2", "1"]
+
+    assert {:invalid} = AMI.Action.add_field(map, "", "100")
+  end
+
+  test "#new with only action" do
+    assert {:ok, map} = AMI.Action.new("Agents")
+    assert map_size(map) == 2
+  end
+
+  test "#new single field" do
+    assert {:ok, map} = AMI.Action.new("Agents", [])
+    assert map_size(map) == 2, "automatically adds action id"
+
+    %{"Action" => [val]} = map
+    assert val == "Agents"
+
+    %{"ActionID" => [val]} = map
+    assert is_bitstring(val) && String.length(val) > 0
+  end
+
+  test "#new with fields list" do
+    assert {:ok, map} =
+             AMI.Action.new("QueueStatus", [
+               {"ActionID", "FOO-bar1"},
+               {"Queue", "Sales"}
+             ])
+
+    %{"Action" => [val]} = map
+    assert val == "QueueStatus"
+
+    %{"ActionID" => [val]} = map
+    assert val == "FOO-bar1"
+
+    %{"Queue" => [val]} = map
+    assert val == "Sales"
+
+    assert {:invalid} = AMI.Action.new("Foo", [{"", ""}])
+  end
+
+  test "#new fail on invalid action" do
+    assert {:invalid} = AMI.Action.new("", [])
+    assert {:invalid} = AMI.Action.new("   ", [])
+    assert {:invalid} = AMI.Action.new(nil, [])
+  end
+
+  test "#to_json" do
+    assert {:ok, map} = AMI.Action.new("Agents", [{"ActionID", "foo0f"}])
+
+    assert ~s(Action: Agents\r\nActionID: foo0f\r\n\r\n) ==
+             AMI.Action.to_string(map)
+
+    assert {:ok, map} = AMI.Action.add_field(map, "Var", "a=1")
+    assert {:ok, map} = AMI.Action.add_field(map, "Var", "b=2")
+
+    assert ~s(Action: Agents\r\nActionID: foo0f\r\n) <>
+             ~s(Var: a=1\r\nVar: b=2\r\n\r\n) ==
+             AMI.Action.to_string(map)
+
+    assert {:ok, map} =
+             AMI.Action.new("Agents", [
+               {"ActionID", "foo"},
+               {"Foo", "123"},
+               {"Bar", "abc"}
+             ])
+
+    assert ~s(Action: Agents\r\nActionID: foo\r\n) <>
+             ~s(Bar: abc\r\nFoo: 123\r\n\r\n) ==
+             AMI.Action.to_string(map)
+  end
+end
